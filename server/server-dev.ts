bdf1a8d09982fcb4fcadbc98921133bf997953ca
louismrtg <louismartignon.2008+github@outlook.com>
2025-09-27 10:59:22 +0200

    Delete server directory

--- server/server-dev.ts
+++
@@ -1,9 +0,0 @@
-import { createServer } from "./index";
-
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-
-const app = createServer();
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
