51ee935beb41ec05fc446a23cd786be971564773
louismrtg <louismartignon.2008+github@outlook.com>
2025-09-27 10:28:17 +0200

    Delete client directory

--- client/pages/OBS.tsx
+++
@@ -1,9 +0,0 @@
-import OBSBanner from "@/components/OBSBanner";
-
-export default function OBSPage() {
-  return (
-    <div className="w-screen h-screen bg-transparent">
-      <OBSBanner autoHideMs={10000} />
-    </div>
-  );
-}
